ce24e817059d3620be54621d2c81ea7a2f1cf0e3
ubuntu <jso1809@naver.com>
2018-10-07 20:23:57 +0900

    +ch4_00

--- caust_web_st/ch3/polls/admin.py
+++ caust_web_st/ch3/polls/admin.py
@@ -3,5 +3,27 @@ from polls.models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 2
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Data information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
