babee584cfaf54657fdddc1071c28ad4ab8e484b
ubuntu <jso1809@naver.com>
2018-10-14 19:15:16 +0900

    add books

---
+++ caust_web_st/ch3/books/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from books.models import Book, Author, Publisher
+
+
+# Create your views here.
+
+# -- TemplateView
+class BooksModelsView(TemplateView):
+    template_name = 'books/index.html'
+
+    def get_context_data(selfself, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_list'] = ['Book', 'Author', 'Publisher']
+        return context
+
+# -- ListView
+class BookList(ListView):
+    model = Book
+
+class AuthorList(ListView):
+    model = Author
+
+class PublisherList(ListView):
+    model = Publisher
+
+# -- DetailView
+class BookDetail(DetailView):
+    model = Book
+
+class AuthorDetail(DetailView):
+    model = Author
+
+class PublisherDetail(DetailView):
+    model = Publisher
